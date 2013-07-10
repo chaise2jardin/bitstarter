72f1e6e25ca9d5bb7b451373dac74336fb20e60a
Ubuntu <ubuntu@ip-172-31-43-54.us-west-2.compute.internal>
2013-07-10 10:52:46 +0000

    I want to commit web.js changes

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(32);
+
+buffer=fs.readFile(index.html);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
